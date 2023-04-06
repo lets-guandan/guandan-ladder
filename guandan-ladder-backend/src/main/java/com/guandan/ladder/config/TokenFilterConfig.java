01471c54dd31be4ab8c0ee4994dc013b6f704135
Hccake <chengbohua@foxmail.com>
2023-04-06 20:54:46 +0800

    :zap: 优化，token 拦截器在校验失败时直接响应，而不是转发

--- guandan-ladder-backend/src/main/java/com/guandan/ladder/config/TokenFilterConfig.java
+++ guandan-ladder-backend/src/main/java/com/guandan/ladder/config/TokenFilterConfig.java
@@ -1,33 +1,35 @@
 package com.guandan.ladder.config;
 
 import cn.hutool.core.codec.Base64;
+import com.hccake.ballcat.common.model.result.R;
+import com.hccake.ballcat.common.util.JsonUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.annotation.Resource;
 import javax.servlet.FilterChain;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * @author yuanjiajia
- * @version 1.0
- * @date 2023/4/3
+ * @author yuanjiajia 2023/4/3
  */
 @Configuration
 public class TokenFilterConfig {
 
 	private static final String TOKEN = "token";
+
 	private static final String LOGIN_PATH = "/login";
-	private static final String LOGIN_EXCEPTION_PATH = "/error/loginException";
+
 	private static final String NONE_PATH = "/";
+
 	@Resource
 	private TokenMap tokenMap = new TokenMap();
 
@@ -50,24 +52,26 @@ public class TokenFilterConfig {
 				if (!LOGIN_PATH.equals(requestUri) && !NONE_PATH.equals(requestUri)) {
 					// 非登录接口需要判断token
 					if (!checkToken(req.getHeader(TOKEN))) {
-						RequestDispatcher rd = req.getRequestDispatcher(LOGIN_EXCEPTION_PATH);
-						rd.forward(req, res);
+						res.setHeader("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+						R<Void> failedResult = R.failed(1000, "未获取到登录信息，请登录");
+						res.getWriter().write(JsonUtils.toJson(failedResult));
 						return;
 					}
 				}
 				try {
-					//将用户id存到本地线程 todo  处理成用户id
+					// 将用户id存到本地线程 todo 处理成用户id
 					UserContext.setUserId(req.getHeader(TOKEN));
 					chain.doFilter(req, res);
-				} finally {
-					//清除本地线程 避免线程复用带来错误
+				}
+				finally {
+					// 清除本地线程 避免线程复用带来错误
 					UserContext.clear();
 				}
 			}
 		};
 	}
 
-	private Boolean checkToken(String token) {
+	private boolean checkToken(String token) {
 		if (StringUtils.isNoneBlank(token)) {
 			// 有token则需要校验是否合法 token：【userName-UUID】
 			String decodeStr = Base64.decodeStr(token);
@@ -77,4 +81,5 @@ public class TokenFilterConfig {
 		}
 		return false;
 	}
+
 }
